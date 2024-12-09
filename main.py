5ddf7fa359a13271f7394f5c89c084b4aa750777
Vaishnavi <vs2753@nyu.edu>
2024-12-09 13:28:31 -0500

    change velocity

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def main():
 
     
     async def findObject(pil_frame, my_detector,base,camera_name):
-        detections = await DO.getDetections(my_detector,camera_name,base,10)
+        detections = await DO.getDetections(my_detector,camera_name,base,100)
         x,y,Xrange,Yrange =  DO.findRange(detections)
         while GO.readyToGrab(pil_frame,Xrange,Yrange)==False:
             asyncio.create_task(DO.motion(pil_frame,my_detector,camera_name, base, 150,15, 500, pil_frame.size[0]/2)) 
