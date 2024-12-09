3a2340cf8539ccdb849dac7a4b4698ce46e10eca
Vaishnavi <vs2753@nyu.edu>
2024-12-09 13:06:01 -0500

    new id

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ import navigateToEP as EP
 
 async def connect():
     opts = RobotClient.Options.with_api_key(
-        api_key='8b19e462-949d-4cf3-9f7a-5ce0854eb7b8',
-        api_key_id='i11ph4btwvdp1kixh3oveex92tmvdtx2'
+        api_key='i11ph4btwvdp1kixh3oveex92tmvdtx2',
+        api_key_id='8b19e462-949d-4cf3-9f7a-5ce0854eb7b8'
     )
     return await RobotClient.at_address('rover6-main.9883cqmu1w.viam.cloud', opts)
 
 async def main():
-    machine = await connect()
+    machine = await connect
     camera_name = "cam"
     camera = Camera.from_robot(machine, camera_name)
     base = Base.from_robot(machine, "viam_base")
